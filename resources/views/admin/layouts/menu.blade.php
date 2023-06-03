6722fdcc09345066828bb9e79001d064e4d1baea
Zabir Emu <zabirraihan570@gmail.com>
2023-06-03 13:09:28 +0600

    logoutp

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -16,6 +16,17 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('shop.create') }}">Shop Create</a>
                 </li>
+                <li class="nav-item">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
+                </li>
 
             </ul>
 
